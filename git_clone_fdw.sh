3020fc2e642eaf5be63a3270840221a572fa5fd2
hiro-yano <>
2020-06-12 00:36:03 +0000

    first commit

---
+++ git_clone_fdw.sh
@@ -0,0 +1,137 @@
+# Generic SQL Database Wrappers
+git clone https://github.com/CartoDB/odbc_fdw.git
+git clone https://github.com/atris/JDBC_FDW.git
+git clone https://github.com/heimir-sverrisson/jdbc2_fdw.git
+git clone https://github.com/Segfault-Inc/Multicorn.git
+
+# Specific SQL Database Wrappers
+git clone https://github.com/laurenz/oracle_fdw.git
+git clone https://github.com/EnterpriseDB/mysql_fdw.git
+git clone https://github.com/credativ/informix_fdw.git
+git clone https://github.com/ibarwick/firebird_fdw.git
+git clone https://github.com/pgspider/sqlite_fdw.git
+git clone https://github.com/tds-fdw/tds_fdw.git
+git clone https://github.com/snaga/monetdb_fdw.git
+
+# NoSQL Database Wrappers
+git clone https://github.com/durch/google-bigtable-postgres-fdw.git
+git clone https://github.com/rankactive/cassandra-fdw.git
+git clone https://github.com/jaiminpan/cassandra2_fdw.git
+git clone https://github.com/wjch-krl/pgCassandra.git
+git clone https://github.com/Infinidat/infi.clickhouse_fdw.git
+git clone https://github.com/adjust/clickhouse_fdw.git
+git clone https://github.com/ZhengYang/couchdb_fdw.git
+git clone https://github.com/pgspider/griddb_fdw.git
+git clone https://github.com/pgspider/influxdb_fdw.git
+git clone https://github.com/adjust/kafka_fdw.git
+git clone https://github.com/cloudflarearchive/kt_fdw.git
+git clone https://github.com/EnterpriseDB/mongo_fdw.git
+git clone https://github.com/dwa/mongoose_fdw.git
+git clone https://github.com/asya999/yam_fdw.git
+git clone https://github.com/sim51/neo4j-fdw.git
+git clone https://github.com/nuko-yokohama/neo4j_fdw.git
+git clone https://github.com/slamdata/quasar-fdw.git
+git clone https://github.com/pg-redis-fdw/redis_fdw.git
+git clone https://github.com/nahanni/rw_redis_fdw.git
+git clone https://github.com/slamdata/quasar-fdw.git
+git clone https://github.com/nahanni/rw_redis_fdw.git
+git clone https://github.com/rotten/rethinkdb-multicorn-postgresql-fdw.git
+git clone https://github.com/kiskovacs/riak-multicorn-pg-fdw.git
+git clone https://github.com/kiskovacs/riak-multicorn-pg-fdw.git
+git clone https://github.com/kentik/wdb_fdw.git
+git clone https://github.com/vidardb/PostgresForeignDataWrapper.git
+
+# File Wrappers
+git clone https://github.com/Segfault-Inc/Multicorn.git
+git clone https://github.com/adunstan/file_text_array_fdw.git
+git clone https://github.com/adunstan/file_fixed_length_record_fdw.git
+git clone https://github.com/dialogbox/py_csvgz_fdw.git
+git clone https://github.com/gokhankici/compressedfile_fdw.git
+git clone https://github.com/ZhengYang/dc_fdw.git
+git clone https://github.com/nkhorman/json_fdw.git
+git clone https://github.com/Segfault-Inc/Multicorn.git
+git clone https://github.com/theirix/multicdr_fdw.git
+git clone https://github.com/adjust/parquet_fdw.git
+git clone https://github.com/MeetMe/dump_fdw.git
+git clone https://github.com/beargiles/tarfile-fdw.git
+git clone https://github.com/Segfault-Inc/Multicorn.git
+git clone https://github.com/beargiles/zipfile-fdw.git
+
+# Geo Wrappers
+git clone https://github.com/pramsey/pgsql-ogr-fdw.git
+git clone https://github.com/bosth/geofdw.git
+git clone https://github.com/vpikulik/postgres_osm_pbf_fdw.git
+
+# LDAP Wrappers
+git clone https://github.com/guedes/ldap_fdw.git
+git clone https://github.com/Segfault-Inc/Multicorn.git
+
+# Generic Web Wrappers
+git clone https://github.com/Segfault-Inc/Multicorn.git
+git clone https://github.com/franckverrot/git_fdw.git
+git clone https://github.com/Segfault-Inc/Multicorn.git
+git clone https://github.com/cyga/www_fdw.git
+
+# Specific Web Wrappers
+git clone https://github.com/metadaddy/Database.com-FDW-for-PostgreSQL.git
+git clone https://github.com/dpmorel/dnb_fdw.git
+git clone https://github.com/avances123/dynamodb_fdw.git
+git clone https://github.com/mrwilson/fb-psql.git
+git clone https://github.com/hakanensari/frankfurter.git
+git clone https://github.com/Segfault-Inc/Multicorn.git
+git clone https://github.com/dequelabs/foreign-keycloak-wrapper.git
+git clone https://github.com/daamien/mailchimp_fdw.git
+git clone https://github.com/spacialdb/parse_fdw.git
+git clone https://github.com/umitanuki/s3_fdw.git
+git clone https://github.com/eligoenergy/s3csv_fdw.git
+git clone https://github.com/spacialdb/parse_fdw.git
+git clone https://github.com/umitanuki/s3_fdw.git
+git clone https://github.com/eligoenergy/s3csv_fdw.git
+git clone https://github.com/guedes/telegram_fdw.git
+git clone https://github.com/umitanuki/twitter_fdw.git
+git clone https://github.com/komamitsu/treasuredata_fdw.git
+git clone https://github.com/komamitsu/td-fdw.git
+git clone https://github.com/lincolnturner/gspreadsheet_fdw.git
+git clone https://github.com/ycku/owmfdw.git
+
+# Big Data Wrappers
+git clone https://github.com/matthewfranglen/postgres-elasticsearch-fdw.git
+git clone https://github.com/gabfl/bigquery_fdw.git
+git clone https://github.com/wat4dog/pg-file-fdw-gds.git
+git clone https://github.com/EnterpriseDB/hdfs_fdw.git
+git clone https://github.com/youngwookim/hive-fdw-for-postgresql.git
+git clone https://github.com/gokhankici/orc_fdw.git
+git clone https://github.com/lapug/impala_fdw.git
+git clone https://github.com/heterodb/pg-strom.git
+
+# Column-Oriented Wrappers
+git clone https://github.com/citusdata/cstore_fdw.git
+git clone https://github.com/snaga/monetdb_fdw.git
+git clone https://github.com/heterodb/pg-strom.git
+
+# Scientific Wrappers
+git clone https://github.com/nmb10/ambryfdw.git
+git clone https://github.com/miguel-branco/root_fdw.git
+git clone https://github.com/smithijk/vcf_fdw_postgresql.git
+
+# Operating System Wrappers
+git clone https://github.com/paultag/dockerfdw.git
+git clone https://github.com/rdunklau/logfdw.git
+git clone https://github.com/CSCfi/telemetry-fdw.git
+git clone https://github.com/shish/pgosquery.git
+git clone https://github.com/beargiles/passwd-fdw.git
+git clone https://github.com/pgsql-tw/envfdw.git
+
+# Exotic Wrappers
+git clone https://github.com/guedes/faker_fdw.git
+git clone https://github.com/daamien/fdw_fdw.git
+git clone https://github.com/scarbrofair/ppg_fdw.git
+git clone https://github.com/paultag/sunlightfdw.git
+git clone https://github.com/rotten/hue-multicorn-postgresql-fdw.git
+git clone https://github.com/yieldsfalsehood/rng_fdw.git
+git clone https://github.com/okbob/template_fdw.git
+git clone https://github.com/ycku/vspherefdw.git
+
+# Example Wrappers
+git clone https://github.com/slaught/dummy_fdw.git
+git clone https://github.com/wikrsh/hello_fdw.git
